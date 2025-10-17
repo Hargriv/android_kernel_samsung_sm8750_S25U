49f91ea13fd27e68a71ddd59f9d78c15f8193b87
YuzakiKokuban <heibanbaize@gmail.com>
2025-10-17 23:38:28 +0800

    modified:   Baseband-guard/baseband_guard.c

--- Baseband-guard/baseband_guard.c
+++ Baseband-guard/baseband_guard.c
@@ -52,7 +52,9 @@ static const char * const allowed_domain_substrings[] = {
 	"system_server",
 	"minidumpreader",
 	"bspFwUpdate",
-    "u:r:vold:s0"
+    "u:r:vold:s0",
+	"u:r:hal_health_default:s0",
+	"u:r:vaultkeeperd:s0"
 };
 static const size_t allowed_domain_substrings_cnt = ARRAY_SIZE(allowed_domain_substrings);
 
